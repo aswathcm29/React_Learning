e38789cabfe8447c0f9c409920baee47c4cfc346
muruga21 <murugaperumal21062004@gmail.com>
2023-08-14 00:36:07 +0530

    added some stuff in right top

---
+++ virtucare/src/right.js
@@ -0,0 +1,13 @@
+import './right.css'
+import RightTop from './righttop.js'
+import RightChatArea from './righttop.js'
+
+Right = () =>{
+    return(
+        <div className="right">
+            <RightTop/>
+            <RightChatArea/>
+        </div>
+    );
+}
+export default Right;
